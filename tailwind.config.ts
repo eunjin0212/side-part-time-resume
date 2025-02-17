8b62d7ab7fee39c220a8ec874f175b8efe4b3f43
EunJin Kim <eunjin9639@gmail.com>
2025-02-17 23:42:03 +0900

    tailwind.config.ts: 추가 화면 크기(xs) 및 슬라이드 애니메이션(keyframes, animation) 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,24 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      screens: {
+        xs: '375px',
       },
+      keyframes: {
+       'slide-in-bottom': {
+         '0%': {
+           transform: 'translateY(100px)',
+           opacity: '0',
+         },
+         '100%': {
+           transform: 'translateY(0)',
+           opacity: '1',
+         }
+       }
+      },
+      animation: {
+       'slide-in-bottom': 'slide-in-bottom 0.8s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
+     },
     },
   },
   plugins: [],
