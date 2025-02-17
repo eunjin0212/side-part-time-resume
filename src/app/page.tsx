087049c8234ce05362db7e2a783c7690f58d1059
EunJin Kim <eunjin9639@gmail.com>
2025-02-17 23:42:09 +0900

    이력서 페이지 및 섹션 컴포넌트 추가, 메타데이터 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,70 @@
-import Image from "next/image";
+import Section from '@/components/Section'
 
-export default function Home() {
+const Home = () => {
+  const COVER_LETTER = [
+    {
+      title: 'Olive Young',
+      part: 'Sales Associate',
+      range: '2019-04 ~ 2020-08',
+      details: [
+        'I excel at understanding customer needs and recommending the right products.',
+        'At Olive Young, I won first place in a sales competition by effectively promoting private brand cosmetics.',
+        'Additionally, I was recognized for outstanding customer service, demonstrating my ability to provide excellent experiences for customers.',
+      ],
+      imgs: [
+        '/assets/oy.jpg',
+        '/assets/oy_sale.jpg',
+        '/assets/oy_competition.jpg',
+      ],
+    },
+    {
+      title: 'Club',
+      part: 'Server',
+      range: '2015-05 ~ 2017-06',
+      details: [
+        'I excel at understanding customer needs and recommending the right products.',
+        'At Olive Young, I won first place in a sales competition by effectively promoting private brand cosmetics.',
+        'Additionally, I was recognized for outstanding customer service, demonstrating my ability to provide excellent experiences for customers.',
+      ],
+      imgs: ['/assets/arena.jpg', '/assets/arena2.jpg', '/assets/mass.jpg'],
+    },
+    {
+      title: 'McDonald`s',
+      part: 'Crew Member',
+      range: '2011-08 ~ 2014-01',
+      details: [
+        'I excel at understanding customer needs and recommending the right products.',
+        'At Olive Young, I won first place in a sales competition by effectively promoting private brand cosmetics.',
+        'Additionally, I was recognized for outstanding customer service, demonstrating my ability to provide excellent experiences for customers.',
+      ],
+      imgs: ['/assets/mcdonald.jpg', '/assets/mcdonald2.jpg'],
+    },
+  ]
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <main>
+      <Section className="w-screen bg-gradient-to-b from-lime-200 to-white text-lime-950">
+        <>
+          <h1 className="pb-10 text-8xl max-md:text-5xl max-sm:text-4xl">
+            Hello, 🌱
+            <br /> My name is <br className="hidden max-md:block" />
+            <b className="max-sm:text-5xl">Eunjin Kim</b>.
+          </h1>
+          <h4 className="pb-10 text-6xl max-md:text-3xl max-sm:text-2xl">
+            Are you looking for someone
+            <br />
+            who provides <b>professional service</b> and makes customers feel
+            great?
+          </h4>
+          <h6 className="text-6xl font-bold max-md:text-5xl max-sm:text-3xl">
+            Here I am! ✋
+          </h6>
+        </>
+      </Section>
+      {COVER_LETTER.map((item) => (
+        <Section key={item.title} {...item} />
+      ))}
+    </main>
+  )
 }
+
+export default Home
---
+++ src/components/Section.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import Image from 'next/image'
+import { ReactNode, useEffect, useRef, useState } from 'react'
+
+interface SectionProps {
+  className?: string
+  children?: ReactNode
+  title?: string
+  range?: string
+  part?: string
+  details?: string[]
+  imgs?: string[]
+}
+const Section = ({ className, children, range, title, part, details, imgs }: SectionProps) => {
+  const ref = useRef<HTMLDivElement | null>(null)
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true)
+        }
+      },
+      { threshold: 0.2 },
+    )
+
+    if (ref.current) observer.observe(ref.current)
+    return () => observer.disconnect()
+  }, [])
+
+  const sectionClass =
+    'py-10 px-7 h-screen lg:py-32 sm:py-12 sm:px-9 [&>div]:max-w-[1260px] [&>div]:mx-auto'
+  return (
+    <section
+      ref={ref}
+      className={[
+        sectionClass,
+        className,
+        
+      ].join(' ')}
+    >
+      <div>
+        {children ? (
+          children
+        ) : (
+          <>
+            <div className={isVisible ? 'animate-slide-in-bottom' : 'translate-y-10 opacity-0'}>
+              <div className="flex flex-row items-end gap-2 max-sm:flex-col max-sm:items-start">
+                <h3 className="text-4xl font-bold">{title}</h3>
+                <span>({part})</span>
+                <em className="inline-block text-gray-500">{range}</em>
+              </div>
+              <ul className='mt-6'>
+                {details?.map((text) => (
+                  <li key={text}>{text}</li>
+                ))}
+              </ul>
+              <div className="mt-6 flex gap-4 h-[700px] w-[800px] flex-row max-md:h-[500px] max-md:w-[600px] max-sm:h-[300px] max-sm:w-[400px]">
+                {imgs?.map((img) => (
+                  <div
+                    key={img}
+                    className="flex-1 overflow-hidden leading-[0] transition-[.5s] hover:flex-[1_1_50%] hover:[&>img]:h-auto hover:[&>img]:w-full"
+                  >
+                    <Image
+                      src={img}
+                      priority={true}
+                      alt="oy"
+                      width={1000}
+                      height={1500}
+                      className="w-[150%] object-cover transition-[.5s]"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </section>
+  )
+}
+
+export default Section
